0100d82bc8bce51d29c3adff4b3f6588ffee5811
chayan2006 <chayankhatua2006@gmail.com>
2025-11-17 21:52:17 +0530

    Activity

--- basics.py
+++ basics.py
@@ -56,7 +56,7 @@ z: str = "Hello, world!"
 # Define a variable of type int
 x: int = 10
 # Define a variable of type float
-y: float = 1.23
+y: float = 1.2
 # Define a variable of type list
 list_of_numbers: typing.List[int] = [1, 2, 3]
 # Define a variable of type tuple
@@ -66,4 +66,18 @@ dictionary: typing.Dict[str, int] = {"key1": 1, "key2": 2}
 # Define a variable of type set
 set_of_numbers: typing.Set[int] = {1, 2, 3}
 
+#Activity 
+# The following lines assign the variable to the left of the = 
+# assignment operator with the values and arithmetic expressions 
+# on the right side of the = assignment operator.
+hotel_room = 100
+tax = hotel_room * 0.08
+total = hotel_room + tax
+room_guests = 4
+share_per_person = total/room_guests
+
+
+# This line outputs the result of the final calculation stored
+# in the variable "share_per_person"
+print("Each person needs to pay: " + str(share_per_person)) # change a data type
 
