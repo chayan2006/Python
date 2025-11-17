56d729c351489aa1fac78d0c6296e7c4c73fc8d0
chayan2006 <chayankhatua2006@gmail.com>
2025-11-17 22:48:00 +0530

    adding new activity

--- basics.py
+++ basics.py
@@ -81,3 +81,10 @@ share_per_person = total/room_guests
 # in the variable "share_per_person"
 print("Each person needs to pay: " + str(share_per_person)) # change a data type
 
+bill = 47.28 # Assign "bill" variable with bill amount
+tip = bill * 0.15 # Multiply by stated tip rate 
+total = bill + tip # Sum the "total" of the "bill" and "tip"
+share = total/2 # Divide "total" by number of friends dining
+print("Each person needs to pay:" + str(share)) # Enter the required string and "share" 
+# Hint: Remember to convert incompatible data types
+
