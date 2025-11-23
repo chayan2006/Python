7376dd736ec1de9b5e9c49a6c21f0e891d6c9b57
chayan2006 <chayankhatua2006@gmail.com>
2025-11-23 22:54:39 +0530

    add new line of code

--- oops.py
+++ oops.py
@@ -26,3 +26,32 @@ class Car:  # Car is the class (blueprint)
 c1 = Car("BMW", "black")  # c1 is an object (instance of Car)
 print(c1.name)    # printing the object's name attribute
 print(c1.colour)  # printing the object's colour attribute
+
+
+Name = input("Enter your name: ")
+Language = input("Enter your language: ")
+Salary = 0 
+if Language == "python":
+    Salary += 1000000
+elif Language == "java":
+    Salary += 1000000000000
+elif Language == "c++":
+    Salary += 100000000000000000
+   
+class Employess:
+    name = Name 
+    language = Language 
+    salary = Salary
+
+Employess1 = Employess()
+
+print(Employess1.name,Employess1.language,Employess1.salary)
+# if Language == "python":
+#         print("salary:30k")
+# elif Language == "C++":
+#           print("salary:50k")
+# elif Language== "java":
+#             print("salary:70k")
+
+
+# more refined code 
