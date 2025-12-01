56bfff7631828ff0d7e9cf0bb7f3fd000129ed33
chayan2006 <chayankhatua2006@gmail.com>
2025-12-01 16:00:45 +0530

    Update modules.py and file.txt2

---
+++ modules.py
@@ -0,0 +1,33 @@
+import os
+
+# Get the directory where this script is located
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Create file paths relative to the script
+file1_path = os.path.join(script_dir, "file.txt")
+file2_path = os.path.join(script_dir, "file.txt2")
+
+f = open(file1_path, "w")
+f.write("mio")
+f.close() # Good practice to close before re-opening
+
+f = open(file1_path, "r")
+print(f.read())
+f.close()
+
+f = open(file2_path, "w")
+f.write("Hello, World!")
+f.close()
+
+f = open(file2_path, "r")
+print(f.read())
+f.close()
+
+f = open(file2_path, "a")
+f.write("Hello, World!")
+f.close()
+
+f = open(file2_path, "r")
+print(f.read())
+f.close()
+
